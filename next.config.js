57314a73d67e383cb8de67f01e136d7a40f8c919
Gemma Gotch <ggotch@gcfund.org>
2022-03-29 16:24:17 +0900

    edit next config to match main project

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+// const runtimeCaching = require('next-pwa/cache')
+
+let envs = {}
+Object.keys(process.env).forEach((v) => {
+  if (v[0] !== '_' && v.indexOf('NODE') === -1) {
+    envs[v] = process.env[v]
+  }
+})
 
 module.exports = withPWA({
+  env: { ...envs },
+  images: {
+    domains: ['something.blob.core.windows.net'],
+  },
   pwa: {
     dest: 'public',
-    runtimeCaching,
+    // runtimeCaching,
+    // scope: '/pwa/',
+    // to prevent lots of console.log spam in dev
+    // use yarn build && yarn start to test pwa behavior
+    disable: process.env.NODE_ENV === 'development',
   },
 })
